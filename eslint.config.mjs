17d52a2693a355ccba56972eb078f5b47e34fe98
Craig P. Motlin <cmotlin@gmail.com>
2025-05-05 18:02:40 -0400

    Turn off eslint rule perfectionist/sort-object-types and perfectionist/sort-objects.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,4 +6,15 @@ import {fileURLToPath} from 'node:url'
 
 const gitignorePath = path.resolve(path.dirname(fileURLToPath(import.meta.url)), '.gitignore')
 
-export default [includeIgnoreFile(gitignorePath), ...oclif, prettier]
+export default [
+  includeIgnoreFile(gitignorePath),
+  ...oclif,
+  prettier,
+  {
+    rules: {
+      'perfectionist/sort-object-types': 'off',
+      'perfectionist/sort-objects': 'off',
+      'perfectionist/sort-classes': 'off',
+    }
+  }
+]
