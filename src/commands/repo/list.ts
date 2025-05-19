ac47e6b3a01d9b5411241d9d8321ddd638db8938
Tifany Tong <tifanytong1234@gmail.com>
2025-05-19 12:24:17 -0400

    Add flag on including user repos + add pagination if there is no limit

--- src/commands/repo/list.ts
+++ src/commands/repo/list.ts
@@ -22,6 +22,7 @@ export default class RepoList extends Command {
     language: Flags.string({char: 'g', description: 'Language filter', multiple: true}),
     'include-forks': Flags.boolean({description: 'Include forked repositories', default: false}),
     'include-archived': Flags.boolean({description: 'Include archived repositories', default: false}),
+    'include-user-repos': Flags.boolean({description: 'Include user repositories', default: false}),
     limit: Flags.integer({char: 'l', description: 'Max repositories'}),
   }
 
@@ -48,6 +49,7 @@ export default class RepoList extends Command {
     limit: number | undefined,
     includeForks: boolean,
     includeArchived: boolean,
+    includeUserRepos: boolean, 
     execa: typeof execa_,
   ): Promise<z.infer<typeof this.repositoriesSchema>> {
     this.log(`├──╮ 🔍 Fetching GitHub repositories${username ? ` for user: ${username}` : ''}`)
@@ -60,6 +62,13 @@ export default class RepoList extends Command {
 
       if (username) {
         query += `user:${username} `
+      }
+      else if (!includeUserRepos) {
+        const orgsQuery = ['api', '-X', 'GET', '--paginate', '/organizations', '--jq', '.[].login']
+        const {stdout} = await execa('gh', orgsQuery)
+        const orgs = stdout.split('\n').filter(Boolean)
+        const usersToAdd = orgs.map(org => `user:${org}`).join(' ')
+        query += usersToAdd 
       }
 
       query += `${topicQueries} ${languageQueries}`
@@ -82,6 +91,9 @@ export default class RepoList extends Command {
       if (limit) {
         args.splice(6, 0, '-f', `per_page=${limit}`)
         this.log(`│  │  │ Limit: ${limit}`)
+      }
+      else {
+        args.push('--paginate')
       }
 
       const {stdout} = await execa('gh', args)
@@ -157,6 +169,7 @@ export default class RepoList extends Command {
         flags.limit,
         flags['include-forks'],
         flags['include-archived'],
+        flags['include-user-repos'],
         execa,
       )
 
